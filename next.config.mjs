05251a7cc51d0a7bd9a0b7686aebe951682c8acc
Hamid Reza Rahimi <83552539+ihamidrezar@users.noreply.github.com>
2025-07-28 20:31:09 +0330

    Fix PWA setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,18 @@
 import withPWA from 'next-pwa';
 
-// Configuration options for Next.js
-const nextConfig = {
+// Combine Next.js configuration with the PWA plugin
+const nextConfig = withPWA({
   reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true,      // Enable SWC minification for improved performance
+  swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
   },
-};
-
-// Configuration object tells the next-pwa plugin 
-const withPWAConfig = withPWA({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
+  pwa: {
+    dest: 'public', // Destination directory for the PWA files
+    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+  },
 });
 
-// Combine the Next.js configuration with PWA configuration
-const finalConfig = withPWAConfig(nextConfig);
-
-// Export the combined configuration
-export default finalConfig;
+export default nextConfig;
